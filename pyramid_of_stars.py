3241e80bc7d7676ef6cd5348055a5226fdc1fe49
A-Vaibhav <vaibhav.m.ambekar@gmail.com>
2022-01-16 18:55:22 +0530

    Added pyramid program

---
+++ pyramid_of_stars.py
@@ -0,0 +1,21 @@
+from unittest import result
+
+
+def pyramid_of_stars():
+
+    n = int(input("Enter height for pyramid : "))
+
+    k = n-1           # for Inner looop side blank spaces
+
+    for i in range(n):
+        for j in range(k):
+            print(end=" ")    #side blank spaces of pyramid
+        
+        k = k-1
+        for l in range(i+1):
+            print("* ",end="")  # pyramid stars
+        
+        print("\r")             # for new line
+    
+    
+pyramid_of_stars()
